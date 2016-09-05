9d1b15b052a36796f56cb55217e79cf2d9a33a6a
Eccenux <eccenux+git@gmail.com>
2016-09-05 12:50:25 +0200

    mJ: AutocompleteHelper defined using prototype semi-privacy (rather then actual scope privacy)

--- workdir/lib/jquery.mobile.autocomplete.js
+++ workdir/lib/jquery.mobile.autocomplete.js
@@ -26,6 +26,7 @@ function AutocompleteHelper($, $ul, $input, $clear, deferredGet)
 {
 	var _self = this;
 	var LOG = new Logger('AutocompleteHelper');
+	this._LOG = LOG;
 	//LOG.enabled = false;
 
 	// bind
@@ -34,11 +35,6 @@ function AutocompleteHelper($, $ul, $input, $clear, deferredGet)
 			clear();
 		});
 	}
-	/*
-	$input.unbind().keyup(function() {
-		onNewText.call(this);
-	});
-	*/
 	$input.off('.AutocompleteHelper');
 	$input.on('blur.AutocompleteHelper', function(){
 		LOG.info('blur', this);
@@ -47,7 +43,7 @@ function AutocompleteHelper($, $ul, $input, $clear, deferredGet)
 	$input.on('focus.AutocompleteHelper', function(){
 		LOG.info('focus', this);
 		$input.off('keyup.AutocompleteHelper').on('keyup.AutocompleteHelper', function() {
-			onNewText.call(this);
+			_self._onNewText(this);
 		});
 	});
 
@@ -66,116 +62,127 @@ function AutocompleteHelper($, $ul, $input, $clear, deferredGet)
 	 */
 	this.caching = false;
 
-	var cache = {};
-
-	/**
-	 * Clear cache if you change some options that might change autocomplete results.
-	 */
-	this.clearCache = function() {
-		cache = {};
-	};
-
-	/**
-	 * Clear list.
-	 */
-	function clear() {
-		$ul.html( "" );
-		$ul.listview( "refresh" );
-	}
-	// allow public access
-	this.clearList = clear;
-
-	/**
-	 * Insert text of choosen item.
-	 */
-	function onSelect(event) {
-		var text = $(this).text();
-		if (_self.parseSelected) {
-			text = _self.parseSelected(text);
-		}
-		LOG.info('select', text);
-		$input.val(text);
-		clear();
-		event.preventDefault();
-		event.stopPropagation();
-	}
-
-	var escapeRegExp = /[-[\]{}()*+?.,\\^$|#\s]/g;
-
-	// previous text value in user input
-	var prevValue = "";
-
-	/**
-	 * Make an AJAX call when new text is in the input.
-	 *
-	 * @note keypress is fired before the char is added, keyup seem to work better.
-	 */
-	function onNewText() {
-		var value = this.value;
-		// avoid running when e.g. shift/home were pressed (and nothing actully changed)
-		if (prevValue === value) {
-			return;
-		}
-		if (value.length < _self.minLength) {
-			return;
-		}
-		prevValue = value;
-		if (value in cache) {
-			renderList(cache[value], value);
-			return;
-		}
-		deferredGet(value)
-		.done(function(list){
-			LOG.info('list', list);
-			cache[value] = list;
-			renderList(list, value);
-		});
-	}
-	
-	/**
-	 * Render list.
-	 * @param {Array} list A simple array with text values.
-	 * @param {String} text Searched text.
-	 */
-	function renderList(list, text) {
-		var html = "";
-		//var html = (list.length < 1 ? "" : "<li>" + list.join('</li><li>') + "</li>");
-		if (list.length) {
-			var parsedList = [];	// MUST copy so that the list remain unchanged
-			var re = new RegExp("(" + htmlSpecialChars(text).replace(escapeRegExp, "\\$&") + ")", 'gi');
-			for (var i=0; i<list.length; i++) {
-				var value = htmlSpecialChars(list[i]);
-				value = value.replace(re, '<b>$1</b>');
-				parsedList.push(value);
-			}
-			html = "<li>" + parsedList.join('</li><li>') + "</li>";
-		}
-		$ul
-			.html(html)
-			.listview("refresh")
-			.trigger("updatelayout")
-		;
-		$('li', $ul).unbind().click(onSelect);
-	}
-	/**
-	 * Escape HTML special characters.
-	 * @param {String} text Text to transform.
-	 * @returns {String} Transformed text.
-	 */
-	function htmlSpecialChars(text) {
-		if (text === null) {
-			return "";
-		}
-		text = text.toString()
-			.replace(/&/g, '&amp;')
-			.replace(/>/g, '&gt;')
-			.replace(/</g, '&lt;')
-			.replace(/'/g, '&#039;')
-			.replace(/"/g, '&quot;')
-		;
-		return text;
-	};
+	// mappping some params
+	this._$ = $;
+	this._$ul = $ul;
+	this._$input = $input;
+	this._deferredGet = deferredGet;
 }
+
+AutocompleteHelper.prototype._cache  = {};
+
+/**
+ * Clear cache if you change some options that might change autocomplete results.
+ */
+AutocompleteHelper.prototype.clearCache = function() {
+	this._cache = {};
+};
+
+/**
+ * Clear list.
+ */
+AutocompleteHelper.prototype._clear = function() {
+	this._$ul.html( "" );
+	this._$ul.listview( "refresh" );
+};
+// allow public access
+AutocompleteHelper.prototype.clearList = AutocompleteHelper.prototype._clear;
+
+/**
+ * Insert text of choosen item.
+ */
+AutocompleteHelper.prototype._onSelect = function(event, listElement) {
+	var text = this._$(listElement).text();
+	if (this.parseSelected) {
+		text = this.parseSelected(text);
+	}
+	this._LOG.info('select', text);
+	this._$input.val(text);
+	this._clear();
+	event.preventDefault();
+	event.stopPropagation();
+};
+
+AutocompleteHelper.prototype._escapeRegExp = /[-[\]{}()*+?.,\\^$|#\s]/g;
+
+// previous text value in user input
+AutocompleteHelper.prototype._prevValue = "";
+
+/**
+ * Make an AJAX call when new text is in the input.
+ *
+ * @note keypress is fired before the char is added, keyup seem to work better.
+ */
+AutocompleteHelper.prototype._onNewText = function (input) {
+	var value = input.value;
+	var _self = this;
+	// avoid running when e.g. shift/home were pressed (and nothing actully changed)
+	if (_self._prevValue === value) {
+		return;
+	}
+	if (value.length < _self.minLength) {
+		return;
+	}
+	_self._prevValue = value;
+	if (value in _self._cache) {
+		_self._renderList(_self._cache[value], value);
+		return;
+	}
+	_self._deferredGet(value)
+		.done(function(list){
+			_self._LOG.info('list', list);
+			_self._cache[value] = list;
+			_self._renderList(list, value);
+		})
+	;
+};
+	
+/**
+ * Render list.
+ * @param {Array} list A simple array with text values.
+ * @param {String} text Searched text.
+ */
+AutocompleteHelper.prototype._renderList = function (list, text) {
+	var _self = this;
+	var html = "";
+	//var html = (list.length < 1 ? "" : "<li>" + list.join('</li><li>') + "</li>");
+	if (list.length) {
+		var parsedList = [];	// MUST copy so that the list remain unchanged
+		var re = new RegExp("(" + _self._htmlSpecialChars(text).replace(_self._escapeRegExp, "\\$&") + ")", 'gi');
+		for (var i=0; i<list.length; i++) {
+			var value = _self._htmlSpecialChars(list[i]);
+			value = value.replace(re, '<b>$1</b>');
+			parsedList.push(value);
+		}
+		html = "<li>" + parsedList.join('</li><li>') + "</li>";
+	}
+	_self._$ul
+		.html(html)
+		.listview("refresh")
+		.trigger("updatelayout")
+	;
+	this._$('li', _self._$ul).unbind().click(function(event) {
+		_self._onSelect(event, this);
+	});
+};
+/**
+ * Escape HTML special characters.
+ * @param {String} text Text to transform.
+ * @returns {String} Transformed text.
+ */
+AutocompleteHelper.prototype._htmlSpecialChars = function (text) {
+	if (text === null) {
+		return "";
+	}
+	text = text.toString()
+		.replace(/&/g, '&amp;')
+		.replace(/>/g, '&gt;')
+		.replace(/</g, '&lt;')
+		.replace(/'/g, '&#039;')
+		.replace(/"/g, '&quot;')
+	;
+	return text;
+};
 
 /**
  * Add listview as a blacklist element for toolbar show/hide.
